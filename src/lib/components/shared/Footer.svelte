84529e9203c709479fe279475dc949afd6b0ea3d
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2025-11-07 18:34:28 +0100

    eh

--- src/lib/components/shared/Footer.svelte
+++ src/lib/components/shared/Footer.svelte
@@ -133,7 +133,7 @@
 
 			<div class="flex flex-wrap justify-center gap-6 text-sm">
 				<a
-					href="https:/x.com"
+					href="https://x.com"
 					target="_blank"
 					rel="noopener noreferrer"
 					aria-label="X/Twitter"
@@ -152,28 +152,9 @@
 						></path>
 					</svg>
 				</a>
+
 				<a
-					href="#"
-					target="_blank"
-					rel="noopener noreferrer"
-					aria-label="aedIn"
-					class="block text-muted-foreground hover:text-primary"
-				>
-					<svg
-						class="size-6"
-						xmlns="http://www.w3.org/2000/svg"
-						width="1em"
-						height="1em"
-						viewBox="0 0 24 24"
-					>
-						<path
-							fill="currentColor"
-							d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"
-						></path>
-					</svg>
-				</a>
-				<a
-					href="#"
+					href="https://facebook.com"
 					target="_blank"
 					rel="noopener noreferrer"
 					aria-label="Facebook"
@@ -192,33 +173,9 @@
 						></path>
 					</svg>
 				</a>
+
 				<a
-					href="#"
-					target="_blank"
-					rel="noopener noreferrer"
-					aria-label="Threads"
-					class="block text-muted-foreground hover:text-primary"
-				>
-					<svg
-						class="size-6"
-						xmlns="http://www.w3.org/2000/svg"
-						width="1em"
-						height="1em"
-						viewBox="0 0 24 24"
-					>
-						<path
-							fill="none"
-							stroke="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="1.5"
-							d="M19.25 8.505c-1.577-5.867-7-5.5-7-5.5s-7.5-.5-7.5 8.995s7.5 8.996 7.5 8.996s4.458.296 6.5-3.918c.667-1.858.5-5.573-6-5.573c0 0-3 0-3 2.5c0 .976 1 2 2.5 2s3.171-1.027 3.5-3c1-6-4.5-6.5-6-4"
-							color="currentColor"
-						></path>
-					</svg>
-				</a>
-				<a
-					href="#"
+					href="https://instagram.com"
 					target="_blank"
 					rel="noopener noreferrer"
 					aria-label="Instagram"
@@ -238,7 +195,7 @@
 					</svg>
 				</a>
 				<a
-					href="#"
+					href="https://tiktok.com"
 					target="_blank"
 					rel="noopener noreferrer"
 					aria-label="TikTok"

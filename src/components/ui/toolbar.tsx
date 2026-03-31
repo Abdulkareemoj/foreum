814340471b31fee44ae6988af94823b1f3ad8ef9
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-03-31 15:10:04 +0100

    added plate editor ui components

--- src/components/ui/toolbar.tsx
+++ src/components/ui/toolbar.tsx
@@ -1,9 +1,7 @@
-'use client';
+
 
 import * as React from 'react';
-
-import * as ToolbarPrimitive from '@radix-ui/react-toolbar';
-import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+import { Tooltip as TooltipPrimitive, Toolbar as ToolbarPrimitive } from 'radix-ui';
 import { type VariantProps, cva } from 'class-variance-authority';
 import { ChevronDown } from 'lucide-react';
 

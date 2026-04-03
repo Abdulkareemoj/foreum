91dc2bf924b4a7bc438898ef4b046ca42bff418a
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-04-03 17:00:45 +0100

    chore: auth middleware check

---
+++ src/utils/middleware.ts
@@ -0,0 +1,34 @@
+import { redirect } from "@tanstack/react-router"
+import { createMiddleware } from "@tanstack/react-start"
+import { getRequestHeaders } from "@tanstack/react-start/server"
+import { auth } from "~/server/auth"
+
+export const authMiddleware = createMiddleware().server(
+  async ({ next }) => {
+    const headers = getRequestHeaders()
+    const session = await auth.api.getSession({ headers })
+    
+    if (!session) {
+      throw redirect({ to: "/sign-in" })
+    }
+    
+    return await next()
+  }
+)
+
+export const adminMiddleware = createMiddleware().server(
+  async ({ next }) => {
+    const headers = getRequestHeaders()
+    const session = await auth.api.getSession({ headers })
+    
+    if (!session) {
+      throw redirect({ to: "/sign-in" })
+    }
+    
+    if (session.user.role !== 'admin') {
+      throw redirect({ to: "/" })
+    }
+    
+    return await next()
+  }
+)

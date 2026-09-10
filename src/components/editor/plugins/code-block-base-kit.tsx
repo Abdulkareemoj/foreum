a972a7f137681a97e37938aef560e746b51ba069
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-09-10 18:42:21 +0100

    update location

--- src/components/editor/plugins/code-block-base-kit.tsx
+++ src/components/editor/plugins/code-block-base-kit.tsx
@@ -9,7 +9,7 @@ import {
   CodeBlockElementStatic,
   CodeLineElementStatic,
   CodeSyntaxLeafStatic,
-} from '~/components/ui/code-block-node-static';
+} from '~/components/ui/plate/code-block-node-static';
 
 const lowlight = createLowlight(all);
 

dac499704cc5cbe21facc899b08bfda31c1aafe3
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2026-09-17 14:22:10 +0100

    Refactor button elements in DefaultCatchBoundary and NotFound components to use Button component for consistency

--- src/components/DefaultCatchBoundary.tsx
+++ src/components/DefaultCatchBoundary.tsx
@@ -20,14 +20,14 @@ export function DefaultCatchBoundary({ error }: ErrorComponentProps) {
     <div className="min-w-0 flex-1 p-4 flex flex-col items-center justify-center gap-6">
       <ErrorComponent error={error} />
       <div className="flex gap-2 items-center flex-wrap">
-        <button
+        <Button
           onClick={() => {
             router.invalidate()
           }}
           className={`px-2 py-1 bg-gray-600 dark:bg-gray-700 rounded-sm text-white uppercase font-extrabold`}
         >
           Try Again
-        </button>
+            </Button>
         {isRoot ? (
           <Link
             to="/"
